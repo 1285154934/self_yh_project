9c5b22f776430d97c14a44c8737b4fadf2ae4542
WIN-FO3MHO7558E\Administrator <1285154934@qq.com>
2020-05-16 01:08:51 +0800

    springboot

--- yh-manager/yh-manager-service/src/main/java/com/yh/manager/service/App.java
+++ yh-manager/yh-manager-service/src/main/java/com/yh/manager/service/App.java
@@ -1,13 +1,22 @@
 package com.yh.manager.service;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	@GetMapping("/")
+	@ResponseBody
+	public String home(){
+		return "Hello World!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
