09637853975b4711e0e4139e4f225d24177e505c
WIN-FO3MHO7558E\Administrator <1285154934@qq.com>
2020-05-17 01:09:01 +0800

    springboot笔记

--- yh-manager/yh-manager-service/src/main/java/com/yh/manager/service/App.java
+++ yh-manager/yh-manager-service/src/main/java/com/yh/manager/service/App.java
@@ -2,18 +2,20 @@ package com.yh.manager.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
+@RestController
 public class App 
 {
-	@GetMapping("/")
-	@ResponseBody
+	@RequestMapping("/home")
 	public String home(){
-		return "Hello World!";
+		return "蛋蛋 睡不着吧，起来嗨啊，哈哈哈哈";
 	}
 	
 	public static void main(String[] args) {
